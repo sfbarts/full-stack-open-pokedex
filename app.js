b22c333e196e73a8f33e3aabcdcf0a3d96492faf
sfbarts <sfbedu@gmail.com>
2023-11-07 15:18:28 -0500

    Remove error test health check

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  throw new Error('testing error')
-  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
